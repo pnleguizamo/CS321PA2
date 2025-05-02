e761503a641bf6833fddd46f3e0054250ba7ea64
pnleg <pnleg@pyrite-n3.cs.iastate.edu>
2025-05-02 14:36:37 -0500

    Complete i-type disassembler function

--- disassembler.py
+++ disassembler.py
@@ -50,11 +50,40 @@ def disassemble_r_type(binary_instruction):
     else:
         return "UNKNOWN"
 
+def disassemble_i_type(binary_instruction):
+    opcode = (binary_instruction >> 21) & 0x7FF  # Extract opcode bits
+    
+    rd = (binary_instruction >> 0) & 0x1F
+    rn = (binary_instruction >> 5) & 0x1F
+    imm = (binary_instruction >> 10) & 0xFFF
+    
+    if opcode == 0x488 or opcode == 0x489: # ADDI instruction
+        return f"ADDI X{rd}, X{rn}, #{imm}"
+    
+    if opcode == 0x490 or opcode == 0x491: # ANDI instruction
+        return f"ANDI X{rd}, X{rn}, #{imm}"
+    
+    if opcode == 0x690 or opcode == 0x691: # EORI instruction
+        return f"EORI X{rd}, X{rn}, #{imm}"
+    
+    if opcode == 0x590 or opcode == 0x591: # ORRI instruction
+        return f"ORRI X{rd}, X{rn}, #{imm}"
+    
+    if opcode == 0x688 or opcode == 0x689: # SUBI instruction
+        return f"SUBI X{rd}, X{rn}, #{imm}"
+    
+    if opcode == 0x788 or opcode == 0x789: # SUBIS instruction
+        return f"SUBIS X{rd}, X{rn}, #{imm}"
+    
+    else:
+        return "UNKNOWN"
+    
+
 f = open(sys.argv[1], "rb")
 while(f):
     byte = f.read(4)
     if not byte:
         break 
     instruction = int.from_bytes(byte)
-    print(disassemble_r_type(instruction))
+    print(disassemble_i_type(instruction))
     # print(hex(instruction))
